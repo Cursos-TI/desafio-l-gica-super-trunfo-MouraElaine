8f5d9fd647cb9de81a1b5796f8757978b2069ad8
MouraElaine <moura_elaine@hotmail.com>
2025-05-24 23:40:32 +0000

    DESAFIO SUPER TRUNFO - TEMA 2 (NIVEL NOVATO)

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -39,5 +39,99 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
+    
+    // Variáveis da carta 1
+    char estado1[3];
+    char codigo1[5];
+    char nomeCidade1[50];
+    unsigned long int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;
+
+    // Variáveis da carta 2
+    char estado2[3];
+    char codigo2[5];
+    char nomeCidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    float densidade2;
+    float pibPerCapita2;
+
+    // Leitura dos dados da carta 1
+    printf("\nCadastro da Carta 1\n");
+    printf("Digite as duas letras do estado: ");
+    scanf(" %s", &estado1);
+
+    printf("Digite o código da carta 1 (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Digite o nome da cidade (sem espaços): ");
+    scanf("%s", nomeCidade1);
+
+    printf("Digite a população: ");
+    scanf("%lu", &populacao1);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB em bilhões de reais: ");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+    // Leitura dos dados da carta 2
+    printf("\nCadastro da Carta 2\n");
+    printf("Digite as duas letras do estado: ");
+    scanf(" %s", &estado2);
+
+    printf("Digite o código da carta 2 (ex: B02): ");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome da cidade (sem espaços): ");
+    scanf("%s", nomeCidade2);
+
+    printf("Digite a população: ");
+    scanf("%lu", &populacao2);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB em bilhões de reais: ");
+    scanf("%f", &pib2);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    // Cálculos da Densidade
+    densidade1 = (float)populacao1 / area1;
+    densidade2 = (float)populacao2 / area2;
+
+    // Cálculos do PIB
+    pibPerCapita1 = (float)(pib1 * 1e9) / populacao1; 
+    pibPerCapita2 = (float)(pib2 * 1e9) / populacao2;
+
+    // Comparação de População
+    printf("\nComparação: População\n");
+
+    if (populacao1 > populacao2) {
+    printf("Carta 1 – %s (%s): %lu \n", nomeCidade1, estado1, populacao1);
+    printf("Carta 2 – %s (%s): %lu \n", nomeCidade2, estado2, populacao2);
+    printf("Carta 1 (%s) venceu!\n", nomeCidade1);
+}   else if (populacao2 > populacao1) {
+    printf("Carta 1 – %s (%s): %lu \n", nomeCidade1, estado1, populacao1);
+    printf("Carta 2 – %s (%s): %lu \n", nomeCidade2, estado2, populacao2);
+    printf("Carta 2 (%s) venceu!\n", nomeCidade2);
+}   else {
+    printf("Carta 1 – %s (%s): %lu \n", nomeCidade1, estado1, populacao1);
+    printf("Carta 2 – %s (%s): %lu \n", nomeCidade2, estado2, populacao2);
+    printf("Empate! Ambas as cidades têm a mesma população.\n");
+}
+
     return 0;
 }
